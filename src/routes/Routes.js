ae2031662fb2becb1c9784bd30023979d3c7c972
Tyler Gajdocek <tylerfrommn@gmail.com>
2021-09-17 14:10:07 -0500

    Initial commit

---
+++ src/routes/Routes.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react'
+import { Text } from 'react-native'
+import { useSelector, useDispatch } from 'react-redux'
+import { authenticate } from 'slices/app.slice'
+import Main from './navigation'
+
+const Routes = () => {
+  const { checked, loggedIn } = useSelector((state) => state.app)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(authenticate({ loggedIn: true, checked: true }))
+  }, [])
+  // rendering
+  if (!checked) return <Text>Loading...</Text>
+  return <Main />
+}
+
+export default Routes
---
+++ src/theme/colors.js
@@ -0,0 +1,16 @@
+const colors = {
+  darkPurple: '#191919',
+  purple: '#343a40',
+  lightPurple: '#eee',
+  lightGrayPurple: '#f7f7fb',
+  pink: '#CC71C2',
+  gray: '#343a40',
+  blue: '#0d6fba',
+  peach: '#FF9C92',
+  green: '#88D848',
+  teal: '#53D3E0',
+  yellow: '#FFE94D',
+  darkGray: '#191919',
+}
+
+export default colors
